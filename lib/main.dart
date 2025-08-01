8a32a5418001c50ef612f1854b67f92bcfb75aed
dip-ayon <102429238+dip-ayon@users.noreply.github.com>
2025-08-02 00:13:05 +0600

    added devider, prime check, ODD/Even check, Modules.

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,13 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Counter App',
       theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.greenAccent),
+        useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -23,7 +22,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
 
   final String title;
 
@@ -38,32 +36,75 @@ class _MyHomePageState extends State<MyHomePage> {
   void _incrementCounter() {
     setState(() {
       _counter++;
-      _actionMessage = 'You Have added 1';
+      _actionMessage = 'You have added 1';
     });
   }
+
   void _decrementCounter() {
     setState(() {
-      _counter--;
-      _actionMessage = 'You Have Reduced 1';
-
+      if (_counter > 0) {
+        _counter--;
+        _actionMessage = 'You have Reduced 1';
+      } else {
+        _actionMessage = 'Counter cannot be Negative';
+      }
     });
   }
+
   void _multipleByTwoCounter() {
     setState(() {
       _counter = _counter * 2;
-      _actionMessage = 'You Have Multiplied by 2';
+      _actionMessage = 'You have Multiplied by 2';
     });
   }
+
   void _resetCounter() {
     setState(() {
       _counter = 0;
-      _actionMessage = 'Counter Reset to 0';
+      _actionMessage = 'Counter reset to 0';
+    });
+  }
+  void _divideByTenCounter() {
+    setState(() {
+      _counter = _counter ~/ 10; // integer division
+      _actionMessage = 'You have Divided by 10';
+    });
+  }
+  void _checkOddEven() {
+    setState(() {
+      if (_counter % 2 == 0) {
+        _actionMessage = '$_counter is Even Number';
+      } else {
+        _actionMessage = '$_counter is Odd Number';
+      }
+    });
+  }
+
+  void _checkPrime() {
+    setState(() {
+      if (_counter < 2) {
+        _actionMessage = '$_counter is Not Prime Number';
+        return;
+      }
+      for (int i = 2; i <= _counter ~/ 2; i++) {
+        if (_counter % i == 0) {
+          _actionMessage = '$_counter is Not Prime Number';
+          return;
+        }
+      }
+      _actionMessage = '$_counter is Prime Number';
+    });
+  }
+
+  void _checkModulus() {
+    setState(() {
+      int mod = _counter % 5;
+      _actionMessage = '$_counter % 5 = $mod \nRemainder will be: $mod';
     });
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -74,46 +115,100 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-                _actionMessage,
-                style: const TextStyle(fontSize: 15, color: Colors.black)),
-            Text(
               '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              style: const TextStyle(fontWeight: FontWeight.bold,fontSize: 30,color: Colors.green,),
             ),
-
+            Text(
+              _actionMessage,
+              style: const TextStyle(fontWeight: FontWeight.w600,fontSize: 18,color: Colors.black,),
+            ),
           ],
         ),
       ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                FloatingActionButton(
+                  onPressed: _incrementCounter,
+                  tooltip: 'Increment',
+                  child: const Icon(Icons.add),
+                ),
+                const SizedBox(width: 16),
+                FloatingActionButton(
+                  onPressed: _decrementCounter,
+                  tooltip: 'Decrement',
+                  child: const Icon(Icons.remove),
+                ),
+                const SizedBox(width: 16),
+                FloatingActionButton(
+                  onPressed: _multipleByTwoCounter,
+                  tooltip: 'Multiply by 2',
+                  child: const Center(
+                    child: Text('×2', style: TextStyle(fontSize: 20,color: Colors.black,),
+                      textAlign: TextAlign.center,
+                    ),
+                  )
+                ),
+                const SizedBox(width: 16),
+                FloatingActionButton(
+                  onPressed: _divideByTenCounter,
+                  tooltip: 'Divided by 10',
+                  child: const Center(
+                    child: Text('÷10', style: TextStyle(fontSize: 20,color: Colors.black,),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                FloatingActionButton(
+                  onPressed: _resetCounter,
+                  tooltip: 'Reset',
+                  child: const Icon(Icons.refresh),
+                ),
+                const SizedBox(width: 16),
+                FloatingActionButton(
+                  onPressed: _checkOddEven,
+                  tooltip: 'Odd/Even Check',
+                  child: const Center(
+                    child: Text('Odd/Even', style: TextStyle(fontSize: 14,color: Colors.black,),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
 
-
-
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            onPressed: _incrementCounter,
-            tooltip: 'Increment',
-            child: const Icon(Icons.add),
-          ),
-          const SizedBox(width: 16),
-          FloatingActionButton(
-            onPressed: _decrementCounter,
-            tooltip: 'Decrement',
-            child: const Icon(Icons.remove),
-          ),
-          const SizedBox(width: 16),
-          FloatingActionButton(
-            onPressed: _multipleByTwoCounter,
-            tooltip: 'Multiple By Two',
-            child: const Icon(Icons.clear),
-          ),
-          const SizedBox(width: 16),
-          FloatingActionButton(
-            onPressed: _resetCounter,
-            tooltip: 'Reset',
-            child: const Icon(Icons.refresh),
-          ),
-        ],
+                const SizedBox(width: 16),
+                FloatingActionButton(
+                  onPressed: _checkPrime,
+                  tooltip: 'Prime Check',
+                  child: const Center(
+                    child: Text('Prime?', style: TextStyle(fontSize: 16,color: Colors.black,),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),                ),
+                const SizedBox(width: 16),
+                FloatingActionButton(
+                  onPressed: _checkModulus,
+                  tooltip: 'Modulus Check By 5',
+                  child: const Center(
+                    child: Text('% 5', style: TextStyle(fontSize: 20,color: Colors.black,),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
